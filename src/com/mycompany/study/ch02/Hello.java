5856430a1deffe26c7103ae003eefd7e29b7a3bb
usdev <usdev@naver.com>
2022-07-26 16:18:08 +0900

    22.07.26

---
+++ src/com/mycompany/study/ch02/Hello.java
@@ -0,0 +1,24 @@
+package com.mycompany.study.ch02;
+
+public class Hello {
+
+	public static void main(String[] args) {
+		String title = "WOODIE T-Shirt";
+		String category = "Top";
+		int price = 49000;
+		String color = "white";
+		String size = "S";
+		int num = 1;
+		int wish = 1;
+		int totalprice = 49000;
+		String information = "반팔티";
+
+		System.out.println(title + "|" + category);
+		System.out.println("가격:" + price);
+		System.out.println("선택한 컬러:" + color + " /사이즈 " + size);
+		System.out.println("수량:" + num);
+		System.out.println("총 가격:" + totalprice);
+		System.out.println("위시리스트:" + wish + "개 상품설명:" + information);
+		
+	}
+}
