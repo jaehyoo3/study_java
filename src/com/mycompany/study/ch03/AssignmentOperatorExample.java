80845c3de56d1f42839e1724b30adbef06140ce8
usdev <usdev@naver.com>
2022-08-05 17:12:03 +0900

    22.08.05

---
+++ src/com/mycompany/study/ch03/AssignmentOperatorExample.java
@@ -0,0 +1,19 @@
+package com.mycompany.study.ch03;
+
+public class AssignmentOperatorExample {
+
+	public static void main(String[] args) {
+		int result = 5;
+		
+		result +=10;
+//		result = result + 10;	
+		System.out.println("result: " + result);
+		
+		result -=10;
+		System.out.println("result:" + result);
+	
+		result *= 10;
+		System.out.println("result: " + result);
+	}
+
+}
