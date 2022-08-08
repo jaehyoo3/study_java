bec625d01883d0db422f3ab0ccffd52a3b6b0e79
usdev <usdev@naver.com>
2022-08-08 17:37:51 +0900

    22.08.08

--- src/com/mycompany/study/ch04/ifExample2.java
+++ src/com/mycompany/study/ch04/ifExample2.java
@@ -3,22 +3,19 @@ package com.mycompany.study.ch04;
 public class ifExample2 {
 
 	public static void main(String[] args) {
-		int grade = 1;
-		double price = 10000;
+		int grade = 5;
+		int price = 10000;
 		
 		if(grade == 1) {
 			System.out.println("등급: 일반, " + price + " (" + price*0.05 + ")  = (" + (price- price*0.05) + ")할인");
 		} else if(grade == 2) {
-			System.out.println("등급: 브론즈, " + price + "(" + price*0.10 +") 할인");
+			System.out.println("등급: 일반, " + price + " (" + price*0.10 + ")  = (" + (price- price*0.10) + ")할인");
 		} else if(grade == 3) {
-			price = price * 0.15;
-			System.out.println("등급: 실버, " + price + "(" + price*0.15 + ") 할인");
+			System.out.println("등급: 일반, " + price + " (" + price*0.15 + ")  = (" + (price- price*0.15) + ")할인");
 		} else if(grade == 4) {
-			price = price * 0.20;
-			System.out.println("등급: 골드, " + price + "(" + price*0.20 + ") 할인");
+			System.out.println("등급: 일반, " + price + " (" + price*0.20 + ")  = (" + (price- price*0.20) + ")할인");
 		} else if(grade == 5) {
-			price = price * 0.27;
-			System.out.println("등급: 플레티넘, " + price + "(" + price*0.27 +") 할인");
+			System.out.println("등급: 일반, " + price + " (" + price*0.27 + ")  = (" + (price- price*0.27) + ")할인");
 		}
 	}
 
