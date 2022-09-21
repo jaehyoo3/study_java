e274bbb219e887d62a7ab0b67b5d214026f9d7cc
usdev <usdev@naver.com>
2022-09-21 17:17:46 +0900

    22.09.21

--- src/com/mycompany/study/ch08/first/RemoteControl.java
+++ src/com/mycompany/study/ch08/first/RemoteControl.java
@@ -4,10 +4,12 @@ public interface RemoteControl {
 	public static final int MAX_VOLUME = 10;
 	int MIN_VOLUME = 0;
 	
+	//추상 메서드(주)
 	public abstract void turnOn();	
 	void turnOff();
 	void setVolume(int volume);
 	
+	// 디폴트 메서드
 	default void setMute(boolean mute) {
 		if(mute) {
 			System.out.println("무음 처리합니다.");
@@ -15,7 +17,7 @@ public interface RemoteControl {
 			System.out.println("무음 해제합니다.");
 		}
 	}
-	
+	// 스태틱 : 정적
 	static void changeBattery() {
 		System.out.println("건전지를 교환합니다.");
 	}
